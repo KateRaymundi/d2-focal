9fb5eaeb666d735b06ab3ade37dcdea782799fd5
KateRaymundi <kateacristina@outlook.com>
2020-02-04 20:06:20 +0000

    solution for array to convert in string

---
+++ joinList.js
@@ -0,0 +1,12 @@
+const joinList = function(arr){
+var list = ""
+for(let i = 0; i < arr.length;i++){
+  list += arr[i] 
+
+  if(i !== arr.length-1){
+    list += ", "
+  }
+  }
+  return list
+}
+console.log(joinList(["gists", "types", "operators", "iteration", "problem solving"]))
