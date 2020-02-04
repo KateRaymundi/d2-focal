38a9bb6cdf4b5a44629822a2a68aefa49cdfa016
KateRaymundi <kateacristina@outlook.com>
2020-02-04 20:38:16 +0000

    final version after submission

--- joinList.js
+++ joinList.js
@@ -1,12 +1,12 @@
-const joinList = function(arr){
-var list = ""
-for(let i = 0; i < arr.length;i++){
-  list += arr[i] 
+const joinList = function(arr) {
+  let list = "";
+  for (let i = 0; i < arr.length;i++) {;
+    list += arr[i];
 
-  if(i !== arr.length-1){
-    list += ", "
+    if(i !== arr.length-1) {
+      list += ", "
   }
   }
-  return list
-}
-console.log(joinList(["gists", "types", "operators", "iteration", "problem solving"]))
+  return list;
+};
+console.log(joinList(["gists", "types", "operators", "iteration", "problem solving"]));
